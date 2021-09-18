fa4af06b07febf33172fc332ad33eee7ff13152d
kuyo593 <g70579@icloud.com>
2021-09-18 10:07:10 +0900

    'aaaa'

--- GraphChat/main/admin.py
+++ GraphChat/main/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import User,UserImage
+from .models import User,UserImage,Talk,Group,GroupImage
 admin.site.register(User)
 admin.site.register(UserImage)
+admin.site.register(Talk)
+admin.site.register(Group)
+admin.site.register(GroupImage)
 
 
 # Register your models here.
--- GraphChat/main/models.py
+++ GraphChat/main/models.py
@@ -1,23 +1,30 @@
-
 from django.db import models
 from django.contrib.auth.models import User,AbstractUser
 from django.forms.fields import JSONField
-from django.contrib.postgres.fields import ArrayField
+#from django.contrib.postgres.fields import ArrayField
 
 
 
 class User(User):
     talk = JSONField()
 
-#登録　user (username = demia password = hujyi4597  )
+#登録　user (username = demia password = hujyi4597 username=demi password=ty2345ty )
 
 class UserImage(models.Model):
    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_img")
    image = models.ImageField(verbose_name="画像", null=True, blank=True, upload_to="images")
 
 class Talk(models.Model):
-   talk = models.CharField(blank=True,max_length=500)
+   talk = models.CharField(null=False,max_length=500)
    talk_from = models.ForeignKey(User, on_delete=models.CASCADE, related_name="talk_from")
-   time = models.DateTimeField(auto_now_add=True),
-   child_talk_id=ArrayField(models.IntegerField(blank=True),null=True)
+   time = models.DateTimeField(auto_now_add=True)
+   child_talk_id=JSONField()
+
+class Group(models.Model):
+   name=models.CharField(null=False,max_length=36)
+   topic=JSONField()
+
+class GroupImage(models.Model):
+   user = models.OneToOneField(Group, on_delete=models.CASCADE, related_name="group_img")
+   image = models.ImageField(verbose_name="画像", null=True, blank=True, upload_to="images")
 
