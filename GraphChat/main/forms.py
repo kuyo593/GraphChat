6cf05daae99bf0de8b68afa562cfb207ebbf642b
kuyo593 <f214915@outlook.jp>
2021-11-02 20:27:15 +0900

    'placeholder追加'

--- GraphChat/main/forms.py
+++ GraphChat/main/forms.py
@@ -8,10 +8,16 @@ from django.core.exceptions import ValidationError
 from django.core.validators import ( FileExtensionValidator)
 
 class SignUpForm(UserCreationForm):
-   img = forms.ImageField( required=False, validators=[FileExtensionValidator(["jpg", "jpeg", "png"])],)
-   class Meta:
-    model = User
-    fields = ('username', 'password1', 'password2')
+
+    img = forms.ImageField( required=False, validators=[FileExtensionValidator(["jpg", "jpeg", "png"])],)
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'placeholder':'Name'})
+        self.fields['password1'].widget.attrs.update({'placeholder':'Password'})        
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput(attrs={'class':'validate','placeholder': 'Name'}))
@@ -22,6 +28,6 @@ class TopicForm(forms.Form):
     
 
 class ProfileChangeForm(forms.Form):
-    username = forms.CharField(label='username', required=False)
+    username = forms.CharField(label='username', required=False, widget=TextInput(attrs={'class':'validate','placeholder': 'Name'}))
     image = forms.ImageField(label='image', required=False)
-    password = forms.CharField(label='password', required=False)
+    password = forms.CharField(label='password', required=False, widget=PasswordInput(attrs={'placeholder':'Password'}))
